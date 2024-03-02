59bfd2897ba60cf34928842820ad1d8be3136856
paussaanz <paula@sanz.eu>
2024-03-02 13:43:44 +0100

    render url update

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -6,7 +6,7 @@ const INVALID_STATUS_CODES = [401];
 export const createHttp = (useAccessToken = false) => {
   // Si le pongo true, manda el token si le pone false no hay cabecera Authorization
   const http = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: import.meta.env.VITE_API_URL,
   });
 
   if (useAccessToken) {
