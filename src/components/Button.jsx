f50d1b6a8a39b556ca6e1b7b004ee7dc97a6c007
paussaanz <paula@sanz.eu>
2024-03-08 11:49:53 +0100

    button padding changed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import clsx from "clsx";
 
-const Button = ({ type, onClick, children, disabled, outline, extraClassName, active, size, color }) => {
+const Button = ({ type, onClick, children, disabled, outline, extraClassName, active, size, color, padding="px-5" }) => {
     return (
         <div>
             <button
                 type={type}
                 onClick={onClick}
                 className={clsx(`nav-item btn btn-${color} btn-outline-${outline} btn-${size} 
-                    ${active && 'bg-primary text-cream'} text-uppercase matrice-extra-bold px-5`,
+                    ${active && 'bg-primary text-cream'} text-uppercase matrice-extra-bold ${padding}`,
                     extraClassName,
                     { "opacity-50 pe-none": disabled })} >
                 {children}
