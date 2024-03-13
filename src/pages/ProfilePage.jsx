77bad1a3db98a0f9d2463cd012ad692d44d80b58
paussaanz <paula@sanz.eu>
2024-03-13 13:04:28 +0100

    Profile page edit profile logic

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,11 +2,27 @@ import { useContext, useEffect, useState } from "react";
 import AuthContext from "../contexts/AuthContext";
 import Tabbar from "../components/Tabbar";
 import Button from "../components/Button";
+import AlertDialog from "../components/AlertDialog";
+import FormControl from "../components/Form/FormControl";
+import FormInput from "../components/Form/FormInput";
+import { editProfileService } from "../services/AuthService";
 
 const ProfilePage = () => {
-  const [activeTab, setActiveTab] = useState('All Tasks');
   const { fetchCurrentUser, user } = useContext(AuthContext)
+  const [activeTab, setActiveTab] = useState('All Tasks');
+  const [showEditProfile, setShowEditProfile] = useState(false);
+  const [editingProfile, setEditingProfile] = useState({
+    name: user.name,
+    surname: user.surname,
+    email: user.email,
+    username: user.username,
+    password: user.password,
+    imageUrl: user.imageUrl,
+    newPassword: '',
+    confirmNewPassword: ''
+  });
 
+console.log(user)
   const getTasks = () => {
     if (activeTab !== "All Tasks") {
       return user.tasks.filter((task) => {
@@ -21,6 +37,52 @@ const ProfilePage = () => {
     }
   }
 
+  const handleEditingProfile = (e) => {
+    setEditingProfile((prevProfile) => ({
+      ...prevProfile,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const editProfile = () => {
+    if (!validatePasswordUpdate()) {
+      return;
+    }
+
+    const payload = { ...editingProfile };
+    delete payload.confirmNewPassword;
+
+
+    if (payload.newPassword) {
+      payload.password = payload.newPassword;
+      delete payload.newPassword;
+    }
+
+    editProfileService(user._id, payload)
+      .then((updatedUser) => {
+        fetchCurrentUser();
+        setShowEditProfile(false);
+      })
+      .catch(error => {
+        console.error("Failed to update profile:", error);
+      });
+  };
+
+  const validatePasswordUpdate = () => {
+    const { newPassword, confirmNewPassword } = editingProfile;
+    if (!newPassword) {
+      return true;
+    }
+  
+    if (!newPassword || newPassword.length < 8) {
+      return false;
+    }
+    if (newPassword !== confirmNewPassword) {
+      return false;
+    }
+    return true;
+  };
+
   return (
     <div className="container-fluid">
       <div className="row justify-content-center">
@@ -32,7 +94,12 @@ const ProfilePage = () => {
               <h4 className="fs-6 weight-regular pt-4 pb-3">Profile</h4>
             </div>
             <div className="col-auto icons-black">
-              <Button extraClassName="p-3 icon-edit" />
+              <Button
+                extraClassName="p-3 icon-edit"
+                onClick={() =>
+                  setShowEditProfile(true)
+                }
+              />
             </div>
           </div>
           <div className="row align-items-center p-3">
@@ -40,7 +107,7 @@ const ProfilePage = () => {
               <img src={user.imageUrl} className="rounded-circle" style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
             </div>
             <div className="col">
-              <h4 className="h5 weight-regular m-0">Paula Sanz</h4>
+              <h4 className="h5 weight-regular m-0">{user.name} {user.surname}</h4>
               <div className="row">
                 <div className="col-auto">
                   <h4 className="fs-6 weight-regular m-0 py-1">@{user.username}</h4>
@@ -53,12 +120,119 @@ const ProfilePage = () => {
             </div>
           </div>
 
-          <Tabbar getTasks={fetchCurrentUser} tasks={getTasks()}  activeTab={activeTab} setActiveTab={setActiveTab} />
-        
+          <Tabbar getTasks={fetchCurrentUser} tasks={getTasks()} activeTab={activeTab} setActiveTab={setActiveTab} />
+
         </div>
       </div>
 
-    </div>
+      {showEditProfile && <AlertDialog
+        bg_color="cream"
+        body_weight="semi-bold"
+        title="Edit profile"
+        body={
+          <form className="row" key={user._id}>
+            <div className="col-12">
+              <div className="row">
+                <div className="col-6">
+                  <FormControl
+                    text="Name"
+                    htmlFor="name"
+                  >
+                    <FormInput
+                      id="taskName"
+                      name="name"
+                      type="text"
+                      onChange={handleEditingProfile}
+                      value={editingProfile.name}
+                    />
+                  </FormControl>
+                </div>
+                <div className="col-6">
+                  <FormControl
+                    text="Surname"
+                    htmlFor="surname"
+                  >
+                    <FormInput
+                      id="userSurname"
+                      name="surname"
+                      type="text"
+                      onChange={handleEditingProfile}
+                      value={editingProfile.surname}
+                    />
+                  </FormControl>
+                </div>
+                <div className="col-6">
+                  <FormControl
+                    text="Email"
+                    htmlFor="email"
+                  >
+                    <FormInput
+                      id="email"
+                      name="email"
+                      type="email"
+                      onChange={handleEditingProfile}
+                      value={editingProfile.email}
+                    />
+                  </FormControl>
+                </div>
+                <div className="col-6">
+                  <FormControl
+                    text="Username"
+                    htmlFor="username"
+                  >
+                    <FormInput
+                      id="userUsername"
+                      name="username"
+                      type="text"
+                      onChange={handleEditingProfile}
+                      value={editingProfile.username}
+                    />
+                  </FormControl>
+                </div>
+                <div className="col-6">
+                  <FormControl text="New Password" htmlFor="newPassword">
+                    <FormInput
+                      id="newPassword"
+                      name="newPassword"
+                      value={editingProfile.newPassword || ''}
+                      placeholder="Enter new password"
+                      type="password"
+                      onChange={handleEditingProfile}
+                    />
+                  </FormControl>
+                </div>
+                <div className="col-6">
+                  <FormControl text="Confirm New Password" htmlFor="confirmNewPassword">
+                    <FormInput
+                      id="confirmNewPassword"
+                      name="confirmNewPassword"
+                      value={editingProfile.confirmNewPassword || ''}
+                      placeholder="Confirm new password"
+                      type="password"
+                      onChange={handleEditingProfile}
+                    />
+                  </FormControl>
+                </div>
+              </div>
+            </div>
+          </form>
+        }
+        cancelButton={{
+          text: "CLOSE",
+          onClick: () => {
+            setShowEditProfile(false)
+          },
+          type: "submit"
+        }}
+        acceptButton={{
+          text: "ACCEPT",
+          onClick: () => {
+            editProfile()
+          },
+          type: "submit"
+        }}
+      />}
+    </div >
 
 
   );
