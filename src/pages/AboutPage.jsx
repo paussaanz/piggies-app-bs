e3a061e3cec264c3956515786dcdf9f0390606c4
paussaanz <paula@sanz.eu>
2024-03-16 11:43:36 +0100

    about page finished

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,45 +1,30 @@
 import { useState } from 'react';
 
 const AboutPage = () => {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-    const [isHovering, setIsHovering] = useState(false);
-
-    const handleMouseMove = (e) => {
-        setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-
-    const handleMouseEnter = () => {
-        setIsHovering(true);
-    };
-
-    const handleMouseLeave = () => {
-        setIsHovering(false);
-    };
-
     const info = [
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Integrity",
+            body: " At the core of our operations, integrity represents honesty and transparency in all our actions and decisions. We value the trust our clients, employees, and partners place in us."
         },
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Innovation",
+            body: " We strive to be pioneers in our field, constantly seeking new ideas, technologies, and methods that can enhance the quality and efficiency of our products and services. Innovation is not just a goal but a mindset that we cultivate at every level."
         },
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Commitment",
+            body: "We are dedicated to excellence in every aspect of our business. From the quality of our products to customer service, every team member strives to surpass standards and deliver exceptional results."
         },
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Responsibility",
+            body: "We recognize our responsibility towards the community and the environment. We commit to operating sustainably, minimizing our environmental impact, and making a positive contribution to society. "
         },
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Teamwork",
+            body: "We firmly believe that success is best achieved by working together. We foster a collaborative environment where every team member is valued and their opinions heard. By leveraging our diverse skills and perspectives."
         },
         {
-            title: "Lorem ipsum dolor ",
-            body: "Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur "
+            title: "Adaptability",
+            body: "In a rapidly changing business world, the ability to adapt is crucial for survival and growth. We strive to be agile and flexible, responding quickly to new opportunities and challenges. "
         }
     ]
     const cities = [
@@ -64,27 +49,22 @@ const AboutPage = () => {
         {
             name: "Alex Jordan",
             position: "Creative Director",
-            img: "https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710537641/Piggies/kriaqcsq53omn2kxm65p.jpg"
+            img: "https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710580749/content_enqizs.png"
         },
         {
             name: "Jamie Park",
             position: "Digital Marketing Strategist",
-            img: ""
+            img: "https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710580749/content_2_dsmchy.png"
         },
         {
             name: "Taylor Rivera",
             position: "Graphic Designer",
-            img: ""
+            img: "https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710580749/content_3_ehzzch.png"
         },
         {
             name: "Casey Lee",
             position: "Public Relations Manager",
-            img: ""
-        },
-        {
-            name: "Morgan Ellis",
-            position: "Content Writer",
-            img: ""
+            img: "https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710580749/content_1_rmbars.png"
         }
     ]
     return (
@@ -99,11 +79,11 @@ const AboutPage = () => {
                     <img className="img-solapada" style={{ width: '300px', height: '300px', objectFit: 'contain' }} src="https://res.cloudinary.com/dmbtvuj1x/image/upload/v1710571466/Recurso_1logo-mio_exwpbu.png" />
                 </div>
                 <div className="col-12 pb-3">
-                    <h2 className="h3 weight-semi-bold text-center">Lorem ipsum dolor sit amet, sed consectetur adipiscin elit, sed do. Lorem ipsum dolor sit amet, sed consectetur</h2>
+                    <h2 className="h3 weight-semi-bold text-center">Our company's essence is rooted in a steadfast commitment to integrity, relentless innovation, an unwavering pursuit of excellence. </h2>
                 </div>
                 <div className="row py-5">
                     {info.map((info) => (
-                        <div className="col-4 text-center">
+                        <div className="col-4 text-center pt-4">
                             <h4>{info.title}</h4>
                             <p className="col-10 offset-1">{info.body}</p>
                         </div>
@@ -111,17 +91,20 @@ const AboutPage = () => {
                 </div>
                 <div className="col-12 py-5">
                     <h4 className="h3 weight-bold">Meet the team</h4>
-                    <div className="row" >
+                    <div className="row pt-2" >
                         {teamMembers.map((member) => (
                            <div className="member-container col-3">
                            <div 
                              className="member-card" 
-                             style={{ backgroundImage: `url(${member.img})` }}
+                             style={{ backgroundColor:"grey" }}
                            >
                            </div>
-                           <div className="member-name btn btn-outline-primary p-2">
-                             {member.name}
+                           <div className="member-name btn btn-outline-primary p-2 w-100">
+                           <p className="p-0 m-0">{member.name}</p>
+
+                             <p className="p-0 m-0 mt-2 tag ">{member.position}</p>
                            </div>
+                           
                          </div>
                          
 
@@ -132,7 +115,7 @@ const AboutPage = () => {
 
                 <div className="col-12 py-5">
                     <h4 className="h3 weight-bold">Where to find us</h4>
-                    <div className="row">
+                    <div className="row text-center">
                         {cities.map((city) => (
                             <div className="col-3 py-3">
                                 <h4 className="h5">{city.name}</h4>
