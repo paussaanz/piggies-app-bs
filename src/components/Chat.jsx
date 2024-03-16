e54488d959270ad7d692b38a9b164e1dcb9fd991
paussaanz <paula@sanz.eu>
2024-03-16 07:03:25 +0100

    Chat config and styles

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { getAccessToken } from '../stores/AccessTokenStore';
 import { getMessageHistory } from '../services/MessageService';
+import Button from "../components/Button";
+import FormTextArea from './Form/FormTextArea';
 
 const Chat = ({ currentUser, selectedUser }) => {
     const [messages, setMessages] = useState([]);
     const [currentMessage, setCurrentMessage] = useState("");
     const [socket, setSocket] = useState(null);
 
-    const room = [currentUser, selectedUser].sort().join('-');
+    const room = [currentUser, selectedUser.username].sort().join('-');
 
     useEffect(() => {
 
         getMessageHistory(room)
-        .then(DBmessages => {
-          console.log(DBmessages); 
-          setMessages(DBmessages)
-        })
-        .catch(error => console.error('Error al cargar el historial de mensajes:', error));
+            .then(DBmessages => {
+                console.log(DBmessages);
+                setMessages(DBmessages)
+            })
+            .catch(error => console.error('Error al cargar el historial de mensajes:', error));
         const newSocket = io.connect("http://localhost:3000", {
             auth: {
                 token: getAccessToken(),
@@ -37,7 +39,7 @@ const Chat = ({ currentUser, selectedUser }) => {
         });
 
         return () => newSocket.disconnect();
-    }, [room, currentUser, selectedUser]);
+    }, [room, currentUser, selectedUser.username]);
 
     const sendMessage = () => {
         if (!socket || !currentMessage.trim()) return;
@@ -46,11 +48,12 @@ const Chat = ({ currentUser, selectedUser }) => {
             room,
             content: currentMessage,
             from: currentUser,
-            to: selectedUser,
+            to: selectedUser.username,
         };
 
         socket.emit('send_message', message);
         setCurrentMessage("");
+        console.log(message, "mensajeee")
     };
 
     const handleMessageChange = (event) => setCurrentMessage(event.target.value);
@@ -63,20 +66,59 @@ const Chat = ({ currentUser, selectedUser }) => {
     };
 
     return (
-        <div>
-            <h2>Chat with {selectedUser}</h2>
-            <ul>
-                {messages.map((message, index) => (
-                    <li key={index}>{message.from}: {message.content}</li>
-                ))}
-            </ul>
-            <textarea
-                value={currentMessage}
-                onChange={handleMessageChange}
-                onKeyPress={handleKeyPress}
-                placeholder="Write a message..."
-            />
-            <button onClick={sendMessage}>Send</button>
+        <div className="h-100 ">
+            <div className="row h-100 align-content-start">
+                <div className="row align-items-center py-3">
+                    <div className="col-auto">
+                        <img src={selectedUser.imageUrl} style={{ borderRadius: '50%', width: '45px', height: '45px', objectFit: 'cover' }} />
+                    </div>
+                    <div className="col">
+                        <h2 className="m-0 h4">{selectedUser.username}</h2>
+                    </div>
+                </div>
+                <div className="col-12 px-0">
+                    <ul className="bg-black mb-0 p-3 h-chat">
+                        {messages.map((message, index) => (
+                            <li key={index} className={message.from === currentUser ? "current-user" : "other-user"}>
+                                <div className="row align-items-end">
+                                    <div className="col">
+                                        {message.content}
+                                    </div>
+                                    <div className="col-auto">
+                                        <p className="m-0 tag">{message.timestamp.slice(11, 16)}</p>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="col-12 mt-auto">
+                    <div className="row border-top p-3 bg-cream align-items-center">
+                        <div className="col-10">
+
+                            <form className="form-floating">
+                                <FormTextArea
+                                    id="message"
+                                    name="message"
+                                    type="text"
+                                    className="h-100"
+                                    onChange={handleMessageChange}
+                                    rows="1"
+                                    onKeyPress={handleKeyPress}
+                                    value={currentMessage}
+                                    placeholder="MESSAGE"
+                                />
+                            </form>
+                        </div>
+                        <div className="col-auto">
+                            <Button
+                                outline='black'
+                                onClick={sendMessage}>Send</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -10,33 +10,38 @@ function MessagingPage() {
 
   useEffect(() => {
     getUsers()
-      .then(setUsers); // Asume una función fetchUsers que obtiene los usuarios
+      .then(setUsers); 
   }, []);
 
-  // Función para manejar la selección de un usuario para chat
   const handleUserSelect = (user) => {
     setSelectedUser(user);
   };
 
+  console.log("USER", user)
+  console.log("SELECTED EN LA PAGE", selectedUser)
+
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className="container-fluid h-100">
+      <div className="row h-100">
         <div className="col-2">
         </div>
-        <div className="col-10">
-          <div className="row">
-            <div className="col-2">
-              <ul>
+        <div className="col-10 h-100">
+          <div className="row  bg-cream h-100">
+            <div className="col-2 border-end">
                 {users.map((user) => (
-                  <li key={user.id} onClick={() => {
-                    handleUserSelect(user.username)
+                  <div className="row py-2 border-bottom align-items-center" key={user.id} onClick={() => {
+                    handleUserSelect(user)
                   }}>
-                    {user.name}
-                  </li>
+                        <div className="col-auto">
+                          <img src={user.imageUrl} style={{ borderRadius: '50%', width: '55px', height: '55px', objectFit: 'cover' }} />
+                        </div>
+                        <div className="col">
+                          <p className="m-0">@{user.username}</p>
+                        </div>
+                      </div>
                 ))}
-              </ul>
             </div>
-            <div className="col-10 pt-5 ps-5">
+            <div className="col-10 h-100">
               {selectedUser && (
                 <Chat currentUser={user.username} selectedUser={selectedUser} />
               )}
