015039a1ad4b5c71fad602c8165adfd37572e492
paussaanz <paula@sanz.eu>
2024-04-02 10:21:29 +0200

    extraClassName

--- src/components/Form/FormTextArea.jsx
+++ src/components/Form/FormTextArea.jsx
@@ -1,8 +1,8 @@
-const FormTextArea = ({ placeholder, type, name, value, error, onChange, onBlur, onKeyPress, rows }) => {
+const FormTextArea = ({ placeholder, type, name, value, error, onChange, onBlur, onKeyPress, rows, extraClassname }) => {
     return (
         <div>
             <label className="visually-hidden" htmlFor={name}>{placeholder}</label>
-            <textarea type={type} rows={rows} name={name} id={name} value={value} onChange={onChange} onBlur={onBlur} onKeyPress={onKeyPress} className="form-control" placeholder={placeholder} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+            <textarea type={type} rows={rows} name={name} id={name} value={value} onChange={onChange} onBlur={onBlur} onKeyPress={onKeyPress} className={`form-control ${extraClassname}`} placeholder={placeholder} aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
             {error && <div className="mt-2 text-primary">{error}</div>}
         </div>
     );
