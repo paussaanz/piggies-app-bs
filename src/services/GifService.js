1448694b37cafb1e4bbdaa713046c46746520065
paussaanz <paula@sanz.eu>
2024-04-02 10:22:27 +0200

    gif service

---
+++ src/services/GifService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const GIPHY_API_KEY = 'N0ENKlcnDzHD95m8unImE8afTScUj3Pj';
+const giphyHttp = axios.create({
+  baseURL: 'http://api.giphy.com/v1/gifs',
+  params: {
+    api_key: GIPHY_API_KEY,
+  },
+});
+
+export const fetchGifs = (searchTerm) => {
+  return giphyHttp.get('/search', { params: { q: searchTerm } });
+};
