ba2549f32a8a36c4839278b822f9b2171c701496
paussaanz <paula@sanz.eu>
2024-04-05 10:56:36 +0200

    currentUser identified in user bar

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -6,11 +6,11 @@ import AuthContext from '../contexts/AuthContext';
 function MessagingPage() {
   const { user } = useContext(AuthContext);
   const [selectedUser, setSelectedUser] = useState(null);
-  const [users, setUsers] = useState([]);
+  const [dbUsers, setDBUsers] = useState([]);
 
   useEffect(() => {
     getUsers()
-      .then(setUsers); 
+      .then(setDBUsers);
   }, []);
 
   const handleUserSelect = (user) => {
@@ -26,18 +26,20 @@ function MessagingPage() {
         <div className="col-10 h-100">
           <div className="row  bg-cream h-100">
             <div className="col-2 border-end">
-                {users.map((user) => (
-                  <div className="row py-2 border-bottom align-items-center" key={user.id} onClick={() => {
-                    handleUserSelect(user)
-                  }}>
-                        <div className="col-auto">
-                          <img src={user.imageUrl} style={{ borderRadius: '50%', width: '55px', height: '55px', objectFit: 'cover' }} />
-                        </div>
-                        <div className="col">
-                          <p className="m-0">@{user.username}</p>
-                        </div>
-                      </div>
-                ))}
+              {dbUsers.map((dbUser) => (
+                <div className="row py-2 border-bottom align-items-center" key={dbUser.id} onClick={() => {
+                  handleUserSelect(dbUser)
+                }}>
+                  <div className="col-auto">
+                    <img src={dbUser.imageUrl} style={{ borderRadius: '50%', width: '55px', height: '55px', objectFit: 'cover' }} />
+                  </div>
+                  <div className="col">
+                    <p className="m-0">@{dbUser.username}
+                      {dbUser.username === user.username ? " (tú)" : ""}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
             <div className="col-10 h-100">
               {selectedUser && (
